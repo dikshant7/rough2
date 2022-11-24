78fb7abe505846de1935286183906f5a82b96c50
DIkshant Boora <62752878+dikshant7@users.noreply.github.com>
2022-11-24 16:20:12 +0530

    Add files via upload

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -4,11 +4,11 @@ $username="5vBcZXgHgu";
 $password="HhGmU0OQpL";
 $database="5vBcZXgHgu";
 $conn=mysqli_connect($server,$username,$password,$database);
-// if($conn)
-// {
-//     echo " succefully connected";
-// }
-// else{
-//     die("error".mysqli_connect_error());
-// }
+if($conn)
+{
+    echo " succefully connected";
+}
+else{
+    die("error".mysqli_connect_error());
+}
 ?>
